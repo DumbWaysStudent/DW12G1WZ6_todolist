7444f6d23db320fe30e96dbc056a6f3c135b86c5
eternallust <rizkyabdulrachman@gmail.com>
2019-10-07 19:32:35 +0700

    benerin nama function

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      listTask : ['swim','study','sleep','run'],
+      dummy : ['swim','study','sleep','run'],
       valueInput : ''
     };
   }
@@ -15,7 +15,7 @@ export default class App extends PureComponent {
     const prevState = this.state.dummy
     const arrayItem = [...prevState,this.state.valueInput] 
     this.setState({
-      listTask : [...arrayItem]
+      dummy : [...arrayItem]
     })
   }
 
@@ -25,7 +25,7 @@ export default class App extends PureComponent {
       return dummy != item
     })
     this.setState({
-      listTask:[...array]
+      dummy:[...array]
     })
   }
 
